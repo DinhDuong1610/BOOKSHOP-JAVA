1bfbac66a5f8fe0cd5add490ddfa2fc47fa3bc4d
Đính Dương <jenny.180820@gmail.com>
2024-05-29 10:54:28 +0700

    DONE

--- BOOKSHOP_CLIENT/src/module-info.java
+++ BOOKSHOP_CLIENT/src/module-info.java
@@ -5,4 +5,11 @@
  * 
  */
 module BOOKSHOP_CLIENT {
+	requires java.desktop;
+	requires miglayout;
+	requires json;
+	requires jcalendar;
+	requires java.sql;
+	requires com.google.zxing;
+	requires com.google.zxing.javase;
 }
--- BOOKSHOP_SERVER/src/module-info.java
+++ BOOKSHOP_SERVER/src/module-info.java
@@ -5,4 +5,12 @@
  * 
  */
 module BOOKSHOP_SERVER {
+	requires java.sql;
+	requires java.desktop;
+	requires json;
+	requires miglayout;
+	requires jcalendar;
+	requires TimingFramework;
+	requires poi.ooxml;
+	requires java.xml;
 }
